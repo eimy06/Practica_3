c20ca35fd189f59c38977dd79850ade8f104d269
eimy06 <eimylaurafigueroa@gmail.com>
2021-02-12 12:54:29 -0400

    Creacion del archivo Ejercicio_1.cs

---
+++ Ejercicio_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int numero, suma;
+		suma = 0;
+		Console.Write("Ingrese un numero: ");
+		numero = int.Parse(Console.ReadLine());
+		while (numero > 0)
+		{
+			suma = suma + numero;
+			Console.Write("Ingrese un numero: ");
+			numero = int.Parse(Console.ReadLine());
+
+		}
+		Console.WriteLine("La suma de los numeros ingresados es: {0}", suma);
+
+	}
+}
