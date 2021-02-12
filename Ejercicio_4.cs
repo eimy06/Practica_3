d0c0b74c9b31e6f6d306fee4bd3639311eb0f906
eimy06 <eimylaurafigueroa@gmail.com>
2021-02-12 13:12:25 -0400

    Creacion del archivo Ejercicio_4.cs

---
+++ Ejercicio_4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int inti = 0;
+		for (inti = 90; inti >= 65; inti--)
+		{
+			Console.WriteLine("Letra: " + Convert.ToString((char)inti));
+		}
+	}
+}
