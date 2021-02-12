f556cf0ef73c1e6dd53b27c4769392c31cf3cd65
eimy06 <eimylaurafigueroa@gmail.com>
2021-02-12 13:07:39 -0400

    Creacion del archivo Ejercicio_3

---
+++ Ejercicio_3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int producto, i;
+		producto = 10 * 10;
+		for (i = producto; i <= 120; i += 2)
+		{
+			Console.WriteLine(i);
+		}
+
+	}
+}
